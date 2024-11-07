cdde971e54750a99c45b7f6fd56c525a96ffeaa3
Ruslan Akhmedov <etzheded@gmail.com>
2024-11-07 19:25:37 +0300

    feat: добавил блок header с навигацией и логотипом

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import styles from "./Header.module.scss";
+
+interface MenuItem {
+  label: string;
+  url: string;
+}
+
+interface MenuData {
+  logo: string;
+  header: MenuItem[];
+}
+
+export function Header() {
+  const [data, setData] = useState<MenuData | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/menu")
+      .then((response) => response.json())
+      .then(setData)
+      .catch((error) => console.error("Ошибка при загрузке данных:", error));
+  }, []);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__logo}>
+        <img className={styles.header__logo_img} src="image/logo-icon.svg" alt="Логотип" />
+      </div>
+      <nav className={styles.header__nav}>
+        {data?.header.map((item) => (
+          <a href={item.url} key={item.label} className={styles.header__nav_item}>
+            {item.label}
+          </a>
+        ))}
+      </nav>
+      <div className={styles.header__right_menu}>
+        <div className={styles.header__search}>
+          <button className={styles.header__search_icon}>
+            <img className={styles.header__search_img} src="image/header/search-icon.svg" alt="Поиск" />
+          </button>
+        </div>
+        <div className={styles.header__link}>
+          <p className={styles.header__link_text}>EBAC</p>
+          <img className={styles.header__link_img} src="image/header/link.svg" alt="Ссылка" />
+        </div>
+      </div>
+    </header>
+  );
+}

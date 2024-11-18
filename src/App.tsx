030906bbb077ce96a11a1a0c29953d91c686c106
Ruslan Akhmedov <etzheded@gmail.com>
2024-11-18 13:50:34 +0300

    feat: добавлена темная тема
    а так же исправлены отступы в компонентах для соответствия PP

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import Articles from "./components/Articles/Articles";
 import Webinars from "./components/Webinars/Webinars";
 import Subscribe from "./components/Subscribe/Subscribe";
 import Footer from "./components/Footer/Footer";
+import style from "./App.module.scss";
 
 export function App() {
   return (
-    <>
+    <div className={style.app}>
       <Header />
       <GamingSection />
       <Articles />
       <Webinars />
       <Subscribe />
       <Footer />
-    </>
+    </div>
   );
 }

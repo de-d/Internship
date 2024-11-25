95ace555a5a9cfd1ec75ac77c9e50c4fa4d497a2
Ruslan Akhmedov <etzheded@gmail.com>
2024-11-25 20:19:53 +0300

    feat: добавлена страница результатов игр
    - Добавлена возможность перехода на главную страницу и страницу настроек через кнопки
    - Добавлен компонент ResultsTable для отображения результатов

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,3 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import ResultsTable from "./Table";
+import style from "./Results.module.scss";
+
 export default function Results() {
-  return <div>Results</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={style.results}>
+      <div className={style.results__container}>
+        <div className={style.results__route}>
+          <button className={style.results__route__button} onClick={() => navigate("/")}>
+            Игровое поле
+          </button>
+          <button className={style.results__route__button} onClick={() => navigate("/settings")}>
+            Настройки
+          </button>
+        </div>
+        <div className={style.results__content}>
+          <div className={style.results__header}>
+            <h1>Результаты игр</h1>
+          </div>
+          <ResultsTable />
+        </div>
+      </div>
+    </div>
+  );
 }

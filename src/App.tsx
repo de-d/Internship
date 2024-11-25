bef6f23444c57eae098aa2a0069c86899b447773
Ruslan Akhmedov <etzheded@gmail.com>
2024-11-25 19:54:31 +0300

    feat: добавлена тёмная и слетлая тема оформления

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 import PlayingField from "./pages/PlayingField/PlayingField";
 import Settings from "./pages/Settings/Settings";
 import Results from "./pages/Results/Results";
+import style from "./styles/App.module.scss";
 
 function App() {
+  const { theme } = useContext(AppContext) || {};
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PlayingField />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </Router>
+    <div className={`${style.app} ${theme === "Light" ? style["light-theme"] : style["dark-theme"]}`}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PlayingField />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 

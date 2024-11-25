7c54ce1eb4b483b3e96c0db97c53b0c4468ef189
Ruslan Akhmedov <etzheded@gmail.com>
2024-11-25 19:56:50 +0300

    refactor: удалён div

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import style from "./styles/App.module.scss";
 import { AppProvider } from "./context/AppContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
-      <div className={style.app}>
-        <App />
-      </div>
+      <App />
     </AppProvider>
   </StrictMode>
 );
